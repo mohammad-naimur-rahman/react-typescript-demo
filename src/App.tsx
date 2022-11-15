f4f9cc0d53a7b2f9f62781bb76c51b83252546fa
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2022-11-16 02:03:55 +0600

    added enum

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Greet from './components/Greet'
 import Person from './components/Person'
 import PersonList from './components/PersonList'
+import { Status } from './components/Status'
 
 function App() {
   const personName = {
@@ -30,6 +31,7 @@ function App() {
       <Greet name='Naimur' messageCount={10} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status='success' />
     </div>
   )
 }
